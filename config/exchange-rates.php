19a012e10afb50775ea54890deb536377f76463c
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-28 22:19:47 +0300

    feat(config): create exchange-rates configuration

---
+++ config/exchange-rates.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'banks' => [
+        'monobank' => [
+            'endpoint' => 'https://api.monobank.ua/bank/currency',
+        ],
+
+        'privatbank' => [
+            'base_url' => 'https://api.privatbank.ua/p24api/exchange_rates',
+        ],
+    ],
+
+    'default_pairs' => [
+        ['USD', 'UAH'],
+        ['EUR', 'UAH'],
+    ],
+];
