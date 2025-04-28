740ef81b3ef5f815524937bfa417f8b2a38e5118
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-28 22:53:47 +0300

    fix(resource): add getters and setters for MonobankApiResource

--- src/Resources/API/MonobankApiResource.php
+++ src/Resources/API/MonobankApiResource.php
@@ -4,15 +4,72 @@ declare(strict_types=1);
 
 namespace Vkarchevskyi\ExchangeRates\Data;
 
-final readonly class MonobankApiResource
+final class MonobankApiResource
 {
-    public function __construct(
-        public int $currencyCodeA,
-        public int $currencyCodeB,
-        public int $date,
-        public ?float $rateSell,
-        public ?float $rateBuy,
-        public ?float $rateCross,
-    ) {
+    private int $currencyCodeA;
+    private int $currencyCodeB;
+    private int $date;
+    private ?float $rateSell;
+    private ?float $rateBuy;
+    private ?float $rateCross;
+
+    public function getCurrencyCodeA(): int
+    {
+        return $this->currencyCodeA;
+    }
+
+    public function setCurrencyCodeA(int $currencyCodeA): void
+    {
+        $this->currencyCodeA = $currencyCodeA;
+    }
+
+    public function getCurrencyCodeB(): int
+    {
+        return $this->currencyCodeB;
+    }
+
+    public function setCurrencyCodeB(int $currencyCodeB): void
+    {
+        $this->currencyCodeB = $currencyCodeB;
+    }
+
+    public function getDate(): int
+    {
+        return $this->date;
+    }
+
+    public function setDate(int $date): void
+    {
+        $this->date = $date;
+    }
+
+    public function getRateSell(): ?float
+    {
+        return $this->rateSell;
+    }
+
+    public function setRateSell(?float $rateSell): void
+    {
+        $this->rateSell = $rateSell;
+    }
+
+    public function getRateBuy(): ?float
+    {
+        return $this->rateBuy;
+    }
+
+    public function setRateBuy(?float $rateBuy): void
+    {
+        $this->rateBuy = $rateBuy;
+    }
+
+    public function getRateCross(): ?float
+    {
+        return $this->rateCross;
+    }
+
+    public function setRateCross(?float $rateCross): void
+    {
+        $this->rateCross = $rateCross;
     }
 }
