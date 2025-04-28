4e62945274ddd943fbf9376df27a51887f363de3
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-28 22:20:24 +0300

    feat(repository): update MonobankRepository

--- src/Repositories/MonobankRepository.php
+++ src/Repositories/MonobankRepository.php
@@ -6,51 +6,37 @@ namespace Vkarchevskyi\ExchangeRates\Repositories;
 
 use Illuminate\Container\Attributes\Config;
 use Illuminate\Http\Client\ConnectionException;
-use Illuminate\Http\Client\Factory;
-use JsonException;
+use Symfony\Component\Serializer\SerializerInterface;
 use Throwable;
-use Vkarchevskyi\ExchangeRates\Data\MonobankData;
+use Vkarchevskyi\ExchangeRates\Data\MonobankApiResource;
 use Vkarchevskyi\ExchangeRates\Exceptions\ApiException;
-use Vkarchevskyi\ExchangeRates\Services\WithFetchAndMap;
+use Vkarchevskyi\ExchangeRates\Service\FetchService;
 
 /**
  * @see https://monobank.ua/api-docs/monobank/publichni-dani/get--bank--currency
  */
 final readonly class MonobankRepository implements BankRepositoryInterface
 {
-    /** @use WithFetchAndMap<MonobankData> */
-    use WithFetchAndMap;
-
     public function __construct(
-        private Factory $http,
-        #[Config('services.monobank.base_url')]
-        private string $baseUrl,
-        #[Config('services.monobank.endpoints.currency')]
-        private string $endpointUrl
+        private FetchService $fetch,
+        #[Config('exchange-rates.banks.monobank.endpoint')]
+        private string $endpoint,
+        private SerializerInterface $serializer,
     ) {
     }
 
     /**
-     * @return MonobankData[]
+     * @return MonobankApiResource[]
      * @throws ConnectionException
-     * @throws JsonException
      * @throws Throwable
      * @throws ApiException
      */
     public function getRates(): array
     {
-        return $this->fetchAndMap(
-            $this->endpointUrl,
-            static function (object $rate): MonobankData {
-                return new MonobankData(
-                    $rate->currencyCodeA,
-                    $rate->currencyCodeB,
-                    $rate->date,
-                    $rate->rateSell ?? null,
-                    $rate->rateBuy ?? null,
-                    $rate->rateCross ?? null,
-                );
-            }
+        return $this->serializer->deserialize(
+            $this->fetch->run($this->endpoint),
+            MonobankApiResource::class . '[]',
+            'json'
         );
     }
 }
